313c028696c99773c11455396f1d5c9372540b77
ingibjorgj10 <i.johannsdottir@gmail.com>
2013-07-17 23:17:03 +0000

    API and Web modified

--- API/ReconesseGame/Game/api.py
+++ API/ReconesseGame/Game/api.py
@@ -4,21 +4,31 @@ from tastypie import fields
 
 
 class LevelResource(ModelResource):
+    questions = fields.ToManyField('Game.api.QuestionIDResource', 'question_set', full=True)
     class Meta:
         queryset = Level.objects.all()
         resource_name = 'level'
+        allowed_methods = ['get']
 
+class QuestionIDResource(ModelResource):
+
+    class Meta:
+        queryset = Question.objects.all()
+        resource_name = 'questionIDs'
+        include_resource_uri = False
+        allowed_methods = ['get']
+        fields = ['id']
 
 class QuestionResource(ModelResource):
     answers = fields.ToManyField('Game.api.AnswerResource', 'answer_set', related_name='entry', full=True)
     class Meta:
         queryset = Question.objects.all()
         resource_name = 'question'
-
+        allowed_methods = ['get']
 
 class AnswerResource(ModelResource):
     entry = fields.ToOneField(QuestionResource, 'question')
     class Meta:
         queryset = Answer.objects.all()
         resource_name = 'answers'
-
+        allowed_methods = ['get']
--- Web/game/javascripts/game.js
+++ Web/game/javascripts/game.js
@@ -1,4 +1,7 @@
 var apiUrl = 'http://localhost/django';
+var currentLevel = 1;
+var questionsInLevel;
+var currentAnswers;
 
 $(document).ready(function () {
     $("#chooseCharacterDialog").dialog({
@@ -7,7 +10,7 @@ $(document).ready(function () {
     $("#levelDialog").dialog({
         autoOpen: false
     });
-    $("#L1questionDialog").dialog({
+    $("#questionDialog").dialog({
         autoOpen: false
     });
 
@@ -17,37 +20,94 @@ $(document).ready(function () {
         });
     });
 
-    $("#startGame").click(function () {
+    $(document).on('click', '#startGame', function () {
         $("#chooseCharacterDialog").dialog("close");
-        $(function () {
-            $("#levelDialog").dialog("open");
-        });
-    });
-
-    $("#level1").click(function () {
-        $("#levelDialog").dialog("close");
-        $(function () {
-            $("#L1questionDialog").dialog("open");
-        });
-    });
-
-    $("#test").click(function(){
-
-    $.getJSON(apiUrl + '/api/level/', { format: "json"})
+        $("#questionDialog").dialog("close");
+        $.getJSON(apiUrl + '/api/level/' + currentLevel + '/', { format: "json"})
             .done(function (data) {
-                alert(JSON.stringify(data));
+
+                questionsInLevel = shuffle(data.questions).splice(0,10);
+                console.log(questionsInLevel);
+                console.log(data.questions);
                 var source = $("#level-template").html();
                 var template = Handlebars.compile(source);
-                var html = template(data.objects[0]);
-
+                var html = template(data);
 
                 $("#levelDialog").html(html);
                 $("#levelDialog").dialog("open");
+
             }).fail(function (jqxhr, textStatus, error) {
                 var err = textStatus + ', ' + error;
                 console.log("Request Failed: " + err);
             });
     });
+    $(document).on('click', '#start-level', function () {
+        if (questionsInLevel.length > 0) {
+            var nextQuestionsID = questionsInLevel[0].id;
+
+            questionsInLevel = questionsInLevel.splice(1);
+
+            $.getJSON(apiUrl + '/api/question/' + nextQuestionsID + '/', { format: "json"})
+                .done(function (data) {
+                    currentAnswers = data.answers;
+
+                    var source = $("#question-template").html();
+                    var template = Handlebars.compile(source);
+                    var html = template(data);
+
+                    $("#questionDialog").dialog("close");
+                    $("#levelDialog").dialog("close");
+                    $("#questionDialog").html(html);
+                    $("#questionDialog").dialog("open");
+
+                }).fail(function (jqxhr, textStatus, error) {
+                    var err = textStatus + ', ' + error;
+                    console.log("Request Failed: " + err);
+                });
+        } else {
 
 
+        }
+
+    });
+
+    $(document).on('click', '#answers a', function () {
+        var index = $("#answers a").index(this);
+
+        if (currentAnswers[index].rightAnswer) {
+            $(this).css("color", "green");
+            if (questionsInLevel.length == 0) {
+                currentLevel = currentLevel+1;
+                $("#nextLevel").show();
+            }
+            else {
+                $("#nextQuestion").show();
+            }
+        } else {
+            $(this).css("color", "red");
+        }
+
+    });
 });
+
+//http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array-in-javascript
+function shuffle(array) {
+    var counter = array.length, temp, index;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
